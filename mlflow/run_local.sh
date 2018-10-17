a0d909575b4c992660cbc957bbe99fceb03ea50d
Nikhil Nygaard <nikhil.nygaard@gmail.com>
2018-10-16 17:58:22 -0700

    saving ipv6 changes

--- mlflow/run_local.sh
+++ mlflow/run_local.sh
@@ -2,14 +2,39 @@
 
 # <n> means replace this with an appropriate value
 
+docker rm mlflow
+
 echo "Building MLFlow Container"
 docker build -t mlflow .
 
 echo "Running Container"
-docker run --name mlflow --network mynetwork -p <CONTAINER MLFLOW PORT>:<HOST PORT> \
-    -p <CONTAINER JOB HANDLER PORT>:<HOST PORT> \
-    -e S3_BUCKET_NAME='<YOUR S3 BUCKET NAME>' \
-    -e JDBC_URL='<YOUR JDBC URL>' \
-    -e USER='<DB USER>' -e PASSWORD='<DB PASSWORD>' -e MLFLOW_PORT=<CONTAINER PORT> \
-    -e API_PORT='4041'  -e AWS_ACCESS_KEY_ID='AKIAILC2A7RT2GK5RFBQ' \
-    -e  WS_SECRET_ACCESS_KEY='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' mlflow
+#docker run --name mlflow \
+#	#--network mynetwork \
+#    -p 4040:4040 \
+#	-p 4041:4041 \
+#	-p 4042:4042 \
+#	-e MLFLOW_PORT='4040' \
+#    -e API_PORT='4041' \
+#	-e DASH_PORT='4042' \
+#    -e S3_BUCKET_NAME='s3://amrit-splice' \
+#    -e JDBC_URL='jdbc:splice://amritsaccount-mlflow.splicemachine-qa.io:1527/splicedb;ssl=basic' \
+#    -e USER='splice' \
+#	-e PASSWORD='lYOaWxfQ' \
+#	-e AWS_ACCESS_KEY_ID='AKIAILC2A7RT2GK5RFBQ' \
+#    -e AWS_SECRET_ACCESS_KEY='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' \
+#	mlflow
+
+docker run \
+	--name mlflow \
+	-p 4040:4040 \
+	-p 4041:4041 \
+	-p 4042:4042 \
+	-e DASH_PORT='4042' \
+	-e S3_BUCKET_NAME='s3://amrit-splice' \
+	-e JDBC_URL='jdbc:splice://amritsaccount-mlflow.splicemachine-qa.io:1527/splicedb;ssl=basic' \
+	-e USER='splice' \
+	-e PASSWORD='lYOaWxfQ' \
+	-e MLFLOW_PORT='4040' \
+	-e API_PORT='4041' \
+	-e AWS_ACCESS_KEY_ID='AKIAILC2A7RT2GK5RFBQ' \
+	-e AWS_SECRET_ACCESS_KEY='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' mlflow
