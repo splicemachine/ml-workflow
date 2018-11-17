2dd056953ed8a71f9499aa16f457a1e5f5eb27ac
Nikhil Nygaard <nikhil.nygaard@gmail.com>
2018-11-16 17:46:13 -0800

    redacting keys for rotation

--- mlflow/run_local.sh
+++ mlflow/run_local.sh
@@ -8,22 +8,6 @@
 #docker build -t mlflow .
 
 #echo "Running Container"
-#docker run --name mlflow \
-#	#--network mynetwork \
-#    -p 4040:4040 \
-#	-p 4041:4041 \
-#	-p 4042:4042 \
-#	-e MLFLOW_PORT='4040' \
-#    -e API_PORT='4041' \
-#	-e DASH_PORT='4042' \
-#    -e S3_BUCKET_NAME='s3://amrit-splice' \
-#    -e JDBC_URL='jdbc:splice://amritsaccount-mlflow.splicemachine-qa.io:1527/splicedb;ssl=basic' \
-#    -e USER='splice' \
-#	-e PASSWORD='lYOaWxfQ' \
-#	-e AWS_ACCESS_KEY_ID='AKIAILC2A7RT2GK5RFBQ' \
-#    -e AWS_SECRET_ACCESS_KEY='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' \
-#	mlflow
-
 docker run \
 	--name mlflow \
 	-p 4040:4040 \
@@ -33,8 +17,8 @@ docker run \
 	-e S3_BUCKET_NAME='s3://amrit-splice' \
 	-e JDBC_URL='jdbc:splice://amritsaccount-mlflow.splicemachine-qa.io:1527/splicedb;ssl=basic' \
 	-e USER='splice' \
-	-e PASSWORD='lYOaWxfQ' \
+	-e PASSWORD='' \
 	-e MLFLOW_PORT='4040' \
 	-e API_PORT='4041' \
-	-e AWS_ACCESS_KEY_ID='AKIAILC2A7RT2GK5RFBQ' \
-	-e AWS_SECRET_ACCESS_KEY='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' splicemachine/mlflow
+	-e AWS_ACCESS_KEY_ID='' \
+	-e AWS_SECRET_ACCESS_KEY='' splicemachine/mlflow
