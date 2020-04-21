a9948f002fbb10d372f511416adabb99cf071c91
Ben Epstein <ben.epstein97@gmail.com>
2020-04-21 13:38:20 -0400

    fix for keras saving

--- bobby/src/handlers/run_handlers/deserializers.py
+++ bobby/src/handlers/run_handlers/deserializers.py
@@ -2,9 +2,11 @@ from tempfile import NamedTemporaryFile
 from pickle import loads as load_pickle_string
 
 from pyspark.ml import PipelineModel
-from keras.models import load_model as load_keras_model, Model as KerasModel
+from tensorflow.keras.models import load_model as load_keras_model, Model as KerasModel
+from h5py import File as h5_file
 from mlflow import spark as mlflow_spark, h2o as mlflow_h2o, keras as mlflow_keras, sklearn as mlflow_sklearn
 from h2o import init as h2o_init, load_model as load_h2o_model, shutdown as h2o_shutdown
+from io import BytesIO
 
 __author__: str = "Splice Machine, Inc."
 __copyright__: str = "Copyright 2019, Splice Machine Inc. All Rights Reserved"
@@ -70,7 +72,8 @@ class Deserializers:
         with NamedTemporaryFile() as tmp:
             tmp.write(artifact_stream)
             tmp.seek(0)
-            model: KerasModel = load_keras_model(tmp.name)
+            hfile = h5_file(BytesIO(tmp.name), 'r')
+            model: KerasModel = load_keras_model(hfile)
 
             mlflow_keras.save_model(model, download_path,
                                     conda_env=conda_env)
