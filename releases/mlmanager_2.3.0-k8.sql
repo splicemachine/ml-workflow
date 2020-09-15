f1e5880a516f223b0c4381de610726613e013238
Ben Epstein <ben.epstein97@gmail.com>
2020-09-15 15:39:33 -0400

    update sql

--- releases/mlmanager_2.3.0-k8.sql
+++ releases/mlmanager_2.3.0-k8.sql
@@ -1,4 +1,6 @@
 ALTER TABLE MLMANAGER.ARTIFACTS ADD COLUMN DATABASE_BINARY BLOB(2G);
-UPDATE MLMANAGER.ARTIFACTS SET DATABASE_BINARY=
-SELECT a.MODEL FROM MLMANAGER.MODELS a join MLMANAGER.TAGS t on a.run_uuid=t.run_uuid and a.name=t."value"
-where t."key"='splice.model_name'; 
+insert into  MLMANAGER.ARTIFACTS (run_uuid, name, database_binary, "size", "binary", FILE_EXTENSION)  --splice-properties insertMode=UPSERT 
+select a.run_uuid, b.name, a.model, b."size", b."binary", b.file_extension from mlmanager.models a
+join mlmanager.tags t on a.run_uuid=t.run_uuid
+join mlmanager.artifacts b on b.name=t."value" and b.run_uuid=t.run_uuid
+where t."key" = 'splice.model_name';
