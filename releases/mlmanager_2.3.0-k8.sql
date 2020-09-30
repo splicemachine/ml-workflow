d1dab91c61e54db353804ab2a6afd5fe22ef8c74
Ben Epstein <bepstein@splicemachine.com>
2020-09-30 09:44:41 -0400

    Update mlmanager_2.3.0-k8.sql

--- releases/mlmanager_2.3.0-k8.sql
+++ releases/mlmanager_2.3.0-k8.sql
@@ -4,3 +4,27 @@ select a.run_uuid, b.name, a.model, b."size", b."binary", b.file_extension from 
 join mlmanager.tags t on a.run_uuid=t.run_uuid
 join mlmanager.artifacts b on b.name=t."value" and b.run_uuid=t.run_uuid
 where t."key" = 'splice.model_name';
+drop view mlmanager.live_model_status;
+rename table mlmanager.model_metadata to mlmanager.database_deployed_metadata;
+create view mlmanager.live_model_status as 
+SELECT mm.run_uuid,
+           mm.action,
+           CASE
+               WHEN ((sta.tableid IS NULL
+                      OR st.triggerid IS NULL
+                      OR (mm.triggerid_2 IS NOT NULL
+                          AND st2.triggerid IS NULL))
+                     AND mm.action = 'DEPLOYED') THEN 'Table or Trigger Missing'
+               ELSE mm.action
+           END AS deployment_status,
+           mm.tableid,
+           mm.trigger_type,
+           mm.triggerid,
+           mm.triggerid_2,
+           mm.db_env,
+           mm.db_user,
+           mm.action_date
+    FROM DATABASE_DEPLOYED_METADATA mm
+    LEFT OUTER JOIN sys.systables sta USING (tableid)
+    LEFT OUTER JOIN sys.systriggers st ON (mm.triggerid = st.triggerid)
+    LEFT OUTER JOIN sys.systriggers st2 ON (mm.triggerid_2 = st2.triggerid);
