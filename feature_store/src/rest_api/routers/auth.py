6db57a85c05475f011df2a3d0ddc309ee51e4a74
myles.novick@gmail.com <myles.novick@gmail.com>
2021-02-18 16:27:55 -0500

    deleting a feature

--- feature_store/src/rest_api/routers/auth.py
+++ feature_store/src/rest_api/routers/auth.py
@@ -6,7 +6,7 @@ security = HTTPBasic()
 
 async def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
     if not check_permission(credentials.username,  credentials.password):
-        raise SpliceMachineException(status_code=status.HTTP_401_UNAUTHORIZED, code=ExceptionCodes.NOT_AUTHORIZED,
+        raise SpliceMachineException(status_code=status.HTTP_403_FORBIDDEN, code=ExceptionCodes.NOT_AUTHORIZED,
                             detail="You do not have permission to do this")
 
 def check_permission(username: str, password: str):
