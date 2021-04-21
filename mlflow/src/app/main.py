070e847da36d7e9772002d44c59a129918b4ef5c
Ben Epstein <ben.epstein97@gmail.com>
2021-04-21 18:17:25 -0400

    working with path route (no more UI)

--- mlflow/src/app/main.py
+++ mlflow/src/app/main.py
@@ -68,25 +68,6 @@ def remove_session(response: Response) -> Response:
     return response
 
 
-@APP.context_processor
-def create_global_jinja_variables():
-    """
-    Create a dictionary of global Jinja2
-    variables that can be accessed in any
-    template
-
-    :return: (dict) Dictionary of key/values
-        mapping global variables to the corresponding
-        Jinja Variables
-    """
-    return dict(
-        cloud_environment_name=CLOUD_ENVIRONMENT.name,
-        known_handlers=KnownHandlers,
-        handler_names=HandlerNames,
-        can_deploy=CLOUD_ENVIRONMENT.can_deploy
-    )
-
-
 # Login Configuration
 @LOGIN_MANAGER.user_loader
 def user_loader(username: str) -> User:
@@ -111,52 +92,6 @@ def unauthorized_user():
     return redirect(url_for('login'))
 
 
-# Login Routes
-@APP.route('/login', methods=['GET', 'POST'])
-def login() -> Response:
-    """
-    Show Login HTML to users if GET,
-    otherwise validate credentials against DB
-    :return:
-    """
-    if request.method == 'GET':
-        if current_user.is_authenticated:
-            return redirect('/')
-        return show_html('login.html')
-
-    username: str = request.form['user']
-    if Authentication.validate_auth(username, request.form['pw']):
-        user: User = User(username)
-        login_user(user)
-        return redirect(request.args.get("next") or url_for('home'))
-
-    return show_html('login.html', unauthorized=True)
-
-
-@APP.route("/logout")
-@login_required
-def logout() -> redirect:
-    """
-    Logout the current logged in
-    user
-    :returns: (redirect) redirect to login
-    """
-    logout_user()
-    return redirect(url_for('login'))
-
-
-# Api Routes
-@APP.route('/api/ui/logs', methods=['POST'])
-@login_required
-@HTTP.generate_json_response
-def get_job_logs_ui():
-    """
-    Retrieve the Job Logs for the UI
-    :return: (dict) job logs
-    """
-    return dict(logs=_get_logs(task_id=request.json['task_id']))
-
-
 @APP.route('/api/rest/logs', methods=['POST'])
 @Authentication.basic_auth_required
 @HTTP.generate_json_response
@@ -177,20 +112,6 @@ def _get_logs(task_id):
     job_id = task_id
     job = Session.query(Job).options(load_only("logs")).filter_by(id=job_id).one()
     return job.logs.split('\n')
-
-
-@APP.route('/api/ui/initiate/', methods=['POST'])
-@HTTP.generate_html_in_home_response
-@login_required
-def initiate_job_ui() -> dict:
-    """
-    Initiate a Job from a web form-- redirects
-    to the home page with a message detailing the
-    status of the new job.
-    :return: (dict) output from queue submission
-    """
-    handler: Handler = KnownHandlers.MAPPING.get(request.form['handler_name'].upper())
-    return handler_queue_job(request.form, handler, user=request.form['user'])
 
 
 @APP.route('/api/rest/initiate', methods=['POST'])
@@ -242,191 +163,6 @@ def handler_queue_job(request_payload: dict, handler: Handler, user: str) -> dic
                 job_id=job.id,
                 timestamp=timestamp())  # turned into JSON and returned
 
-
-# UI Routes
-@APP.route('/api/ui/get_monthly_aggregated_jobs', methods=['GET'])
-@HTTP.generate_json_response
-@login_required
-def get_monthly_aggregated_jobs() -> dict:
-    """
-    Get the monthly aggregated job counts
-    for the chart on the main page
-    :return: (dict) response for the javascript to render
-    """
-
-    results: list = list(Session.execute(str(DatabaseSQL.get_monthly_aggregated_jobs)))
-
-    data: defaultdict = defaultdict(lambda: [0] * 12)  # initialize a dictionary
-    # that for every new key, creates an array of 12 zeros: one for every month of the year
-
-    total_count: int = 0
-    for row in results:
-        month, count, user = row
-        total_count += count
-        data[user][int(month - 1)] = count
-
-    return dict(data=data, total=total_count)
-
-
-@APP.route('/api/ui/get_handler_data', methods=['GET'])
-@HTTP.generate_json_response
-@login_required
-def get_handler_data() -> dict:
-    """
-    Get a count of the enabled handlers
-    currently in the database
-    :return: (dict) response containing
-        number of enabled handlers
-    """
-    enabled_handlers_query = Session.query(
-        Handler.name, Handler.enabled
-    ).filter(
-        Handler.modifiable == 1  # don't want access modifiers
-    )
-    results: list = Session.execute(enabled_handlers_query)
-    return dict(data=[tuple(res) for res in results])
-
-
-@APP.route('/api/ui/get_jobs', methods=['POST'])
-@HTTP.generate_json_response
-@login_required
-def get_jobs() -> dict:
-    """
-    As a temporary workaround,
-    we use SQL instead of SQLAlchemy
-    since driver does not support offset yet.
-    Since this is also a fairly complex query--
-    SQL is more efficient
-    :return: (dict) JSON response rendered in front end
-    """
-    job_table: str = "JOBS"
-
-    # Parse Table Order Information
-    order_arg: str = list(filter(lambda key: key.startswith('sort'), request.form))[0]
-    order_value: str = order_arg.split('[')[1].split(']')[0]  # parse sort[column] -> column
-
-    direction_suffix: str = "DESC" if request.form[order_arg] == 'desc' else ''
-    limit: int = int(request.form['rowCount']) if request.form.get('rowCount') != '-1' else 0
-
-    # AJAX from jquery-bootgrid has -1 if user selects no limit
-    if request.form['searchPhrase']:  # query is doing a search on HTML table
-        int_offset: int = 0  # no offset on searches
-        table_query: text = _get_job_search_query(job_table, order_value, direction_suffix, limit,
-                                                  request.form['searchPhrase'])
-    else:  # query is listing
-        int_offset: int = int(request.form['current']) - 1 if request.form.get('current') else 0
-        table_query: text = _get_job_list_query(job_table, order_value, direction_suffix, limit,
-                                                int_offset)
-
-    total_query: text = f"""SELECT COUNT(*) FROM {job_table}"""  # how many pages to make in js
-
-    futures: list = [
-        EXECUTOR.submit(lambda: [_preformat_job_row(row) for row in Session.execute(table_query)]),
-        EXECUTOR.submit(lambda: list(Session.execute(total_query))[0][0])
-    ]
-
-    table_data, total_rows = futures[0].result(), futures[1].result()  # block until we get results
-
-    # Add Job Logs Links
-    for row in table_data:
-        row[TrackerTableMapping.job_logs] = f"<a href='/watch/{row[TrackerTableMapping.id_col]}'>View Logs</a>"
-
-    return dict(rows=table_data,
-                current=int_offset + 1,
-                total=total_rows,
-                rowCount=limit)
-
-
-def _preformat_job_row(job_row: list) -> dict:
-    """
-    Format job row object to have some
-    columns preformatted (<pre></pre> for table rendering
-    :param job_row: (ResultProxy) the Job Row to format
-    :return: (dict) formatted row
-    """
-    job_object: dict = dict(job_row)
-    for column in TrackerTableMapping.preformatted_columns:
-        if column in job_object:
-            job_object[column] = f'<pre>{job_object[column]}</pre>'
-    return job_object
-
-
-def _get_job_search_query(job_table: str, order_col: str, direction: str, limit: int,
-                          search_term: str) -> text:
-    """
-    Get SQL Query to search columns for search string
-
-    :param job_table: (str) Table in DB where jobs are stored
-    :param order_col: (str) column to sort by
-    :param direction: (str) either desc or nothing (ascending)
-    :param limit: (int) number of rows to retrieve
-    :param search_term: (str) term to look for in searchable columns
-    :return: (text) SELECT statement
-    """
-    limit_clause: str = f'FETCH FIRST {limit} ROWS ONLY' if limit > 0 else ''
-
-    filter_clause: str = f" LIKE '%{search_term}%' OR "
-    filter_expression: str = filter_clause.join(TrackerTableMapping.searchable_columns) + filter_clause[:-4]  # cut off
-    # OR on last column
-
-    return text(
-        f"""
-        SELECT {TrackerTableMapping.sql_columns} FROM {job_table}
-        WHERE {filter_expression} 
-        ORDER BY {TrackerTableMapping.DB_MAPPING[order_col]} {direction}
-        {limit_clause}
-        """
-    )
-
-
-def _get_job_list_query(job_table: str, order_col: str, direction: str, limit: int,
-                        offset: int) -> text:
-    """
-    Get JQuery Bootgrid formatted JSON (no search) for
-    rendering in HTML Table for /tracker.
-
-    :param job_table: (str) Table in DB where jobs are stored
-    :param order_col: (str) column to sort by
-    :param direction: (str) either desc or nothing (ascending)
-    :param limit: (int) number of rows to retrieve
-    :param offset: (int) number of rows to skip
-    :return: (text) SELECT statement
-    """
-    limit_clause: str = f'FETCH NEXT {limit} ROWS ONLY' if limit > 0 else ''
-    return text(
-        f"""
-        SELECT {TrackerTableMapping.sql_columns} FROM {job_table}
-        ORDER BY {TrackerTableMapping.DB_MAPPING[order_col]} {direction}
-        OFFSET {offset} ROWS
-        {limit_clause}
-        """
-    )
-
-
-# HTML Routes
-
-# doesn't matter which name is used to generate the URL since both handlers
-# are started from this page
-@APP.route(KnownHandlers.get_url(HandlerNames.enable_service), methods=['GET'])
-@login_required
-def access() -> Response:
-    """
-    Return HTML containing Access Modifier Page
-    :return: (Response) HTML
-    """
-    return show_html('access.html', handlers=KnownHandlers.get_modifiable())
-
-
-@APP.route('/contact', methods=['GET'])
-@login_required
-def contact() -> Response:
-    """
-    Return HTML containing Contact
-    :return: (Response) HTML
-    """
-    return show_html('contact.html')
-
-
 @login_required
 def deploy_csp() -> Response:
     """
@@ -439,48 +175,6 @@ def deploy_csp() -> Response:
     # they need to match the names given to the CloudEnvironments
     # given in ml-workflow-lib/shared/database.py/splice_models.py:KnownHandlers
     return show_html(f'deploy_{CLOUD_ENVIRONMENT.name.lower()}.html')
-
-
-@APP.route(KnownHandlers.get_url(HandlerNames.deploy_k8s))
-@login_required
-def deploy_k8s() -> Response:
-    """
-    Return HTML containing Kubernetes
-    Deployment form
-    :return: (Response)
-    """
-    return show_html('deploy_kubernetes.html')
-
-
-@APP.route(KnownHandlers.get_url(HandlerNames.deploy_database))
-@login_required
-def deploy_database() -> Response:
-    """
-    Return HTML containing Database Deployment form
-    :return: (Response)
-    """
-    return show_html('deploy_database.html')
-
-
-@APP.route('/tracker', methods=['GET'])
-@login_required
-def tracker() -> Response:
-    """
-    Return HTML containing Tracker table
-    :return: (Response) HTML
-    """
-    return show_html('tracker.html')
-
-
-@APP.route('/', methods=['GET'])
-@login_required
-def home() -> Response:
-    """
-    Serves up home page for MLManager
-    Director
-    :return: (Response) HTML
-    """
-    return show_html('index.html')
 
 
 @APP.route('/watch/<int:task_id>', methods=['GET'])
